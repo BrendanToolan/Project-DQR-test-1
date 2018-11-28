59c57c98187e65bd62079ecbaf77e09f914fd22d
BrendanToolan <34661671+BrendanToolan@users.noreply.github.com>
2018-11-28 21:39:59 +0000

    Add files via upload

---
+++ BackEnd/server.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb://User:rumham1@ds125331.mlab.com:25331/project';
+mongoose.connect(mongoDB);
+
+var Schema = mongoose.Schema;
+var postSchema = new Schema({
+    title: String,
+    content: String,
+    emailaddress: String
+})
+var PostModel = mongoose.model('post', postSchema);
+
+
+//Here we are configuring express to use body-parser as middle-ware. 
+app.use(bodyParser.urlencoded({ extended: false })); 
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    });
+    
+app.post('/name', function(req, res){
+    res.send("Hello you sent " +
+    req.body.firstname + " " +
+    req.body.lastname);
+})
+
+app.get('/', function (req, res) {
+   res.send('Hello from Express');
+})
+
+app.post('/api/posts', function(req, res){
+    console.log("post successful");
+    console.log(req.body.title);
+    console.log(req.body.content);
+    console.log(req.body.emailaddress);
+
+    PostModel.create({
+        title: req.body.title,
+        content: req.body.content,
+        emailaddress: req.body.emailaddress
+    });
+    res.send('Item added');
+
+
+})
+
+app.get('/api/posts', function(req, res){
+    PostModel.find(function(err, data){
+        res.json(data);
+    });
+})
+
+app.delete('/api/posts/:id', function(req, res){
+    console.log(req.params.id);
+
+    PostModel.deleteOne({_id:req.params.id},
+    function(err, data)
+    {
+        if(err)
+            res.send(err);
+        res.send(data);
+    })
+})
+
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://%s:%s", host, port)
+})
